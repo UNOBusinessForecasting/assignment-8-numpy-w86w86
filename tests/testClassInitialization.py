66572e68b8f68035575d842aa9e3b4a17f52b17f
w86w86 <54444342+w86w86@users.noreply.github.com>
2024-03-24 01:36:32 -0500

    Update testClassInitialization.py
    import full lib

--- tests/testClassInitialization.py
+++ tests/testClassInitialization.py
@@ -1,6 +1,10 @@
 import unittest
 import json
 import pandas as pd
+import numpy as np
+import sys as system
+import io
+import re
 
 with open("Lesson.ipynb", "r") as file:
     f_str = file.read()
